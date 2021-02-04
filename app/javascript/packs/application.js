23cd0d78ef57caf93045392e9f7d35a5b0332cb0
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-04 15:30:21 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,10 @@
 
 import '@fortawesome/fontawesome-free/js/all';
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
