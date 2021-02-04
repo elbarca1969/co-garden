d28cd57ca31ced5c1226ef0af9ddf18c6b73956d
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-04 15:21:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true, length: { maximum: 30 } do
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :user_name, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  end
+  validates :introduction, length: { maximum: 150 }
+ 
 end
