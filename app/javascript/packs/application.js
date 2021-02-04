503c38d0a03297992813a1f273c4e0b718474b63
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-04 15:34:27 +0900

    プロフィールアイコンにカーソルが合った時メニューバーを表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('./profile')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
