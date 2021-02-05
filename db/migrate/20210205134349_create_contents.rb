b50d676d0005e50dd8bf81bb509984e0d1cdae12
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-05 22:45:41 +0900

    contentsテーブルの作成

--- db/migrate/20210205134349_create_contents.rb
+++ db/migrate/20210205134349_create_contents.rb
@@ -1,7 +1,11 @@
 class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
-
+      t.integer :grade_id, null: false
+      t.integer :subject_id, null: false
+      t.string :category, null: false
+      t.integer :release_id, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_060921) do
+ActiveRecord::Schema.define(version: 2021_02_05_134349) do
+
+  create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "grade_id", null: false
+    t.integer "subject_id", null: false
+    t.string "category", null: false
+    t.integer "release_id", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_02_04_060921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "users"
 end
