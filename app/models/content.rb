171c0da5f2c7adacf6f6720a2aea2beeaa051817
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-05 23:16:35 +0900

    バリデーションの設定

--- app/models/content.rb
+++ app/models/content.rb
@@ -6,6 +6,18 @@ class Content < ApplicationRecord
   has_rich_text :answer
   has_rich_text :explanation
 
+  with_options numericality: { other_than: 1 } do
+    validates :grade_id
+    validates :subject_id
+  end
+
+  with_options presence: true do
+    validates :category
+    validates :question
+    validates :answer
+    validates :explanation
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
   belongs_to :subject
