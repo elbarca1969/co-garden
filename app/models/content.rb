55a41bd0dc40df72db1477ddb9a18b6302d1d442
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-05 23:05:45 +0900

    リッチテキスト使用の宣言

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,10 @@ class Content < ApplicationRecord
 
   belongs_to :user
 
+  has_rich_text :question
+  has_rich_text :answer
+  has_rich_text :explanation
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
   belongs_to :subject
