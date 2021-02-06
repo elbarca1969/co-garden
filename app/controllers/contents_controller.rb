9efbef5cdc1f60474904d0203abb06b8a00fcfaf
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-06 15:12:14 +0900

    indexアクション内に変数@contentsを定義

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,6 +2,7 @@ class ContentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @contents = current_user.contents.order("created_at DESC")
   end
 
   def new
