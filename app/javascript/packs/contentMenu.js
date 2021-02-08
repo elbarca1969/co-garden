c4b04b8f8b426c0ccb959955413713fc7455df6c
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-08 18:27:51 +0900

    メニューバーにカーソルが合った時にメニューを表示する

---
+++ app/javascript/packs/contentMenu.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".content-show-upper-right-icon").on('mouseenter', function(){
+    $(".content-show-upper-right-menu").show();
+  });
+  $(".content-show-upper-right-icon").on('mouseleave', function(){
+    $(".content-show-upper-right-menu").hide();
+  });
+});
