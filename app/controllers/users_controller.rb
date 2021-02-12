d15197e45b6d5e28451367c595a3490ed2f8044e
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-12 15:30:04 +0900

    usersコントローラーにshowアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @contents = @user.contents.order("created_at DESC").includes([:rich_text_question])
+  end
+
 end
