971adf30737a43ecc3fab70a985d8e3be77a9a8c
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 16:41:04 +0900

    set_contentを定義し記述をまとめる

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,19 @@
 class AnswersController < ApplicationController
+  before_action :set_content, only: [:create, :destroy]
 
   def create
-    @content = Content.find(params[:content_id])
     @answer = Answer.create(user_id: current_user.id, content_id: @content.id)
   end
 
   def destroy
-    @content = Content.find(params[:content_id])
     @answer = Answer.find_by(user_id: current_user.id, content_id: @content.id)
     @answer.destroy
+  end
+
+  private
+
+  def set_content
+    @content = Content.find(params[:content_id])
   end
 
 end
