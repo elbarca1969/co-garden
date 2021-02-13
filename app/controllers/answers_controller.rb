7d34c72d14aa6cc7f4696bd5769a96b4ae7032cf
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 16:40:31 +0900

    answersコントローラーにdestroyアクションを定義

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,4 +5,10 @@ class AnswersController < ApplicationController
     @answer = Answer.create(user_id: current_user.id, content_id: @content.id)
   end
 
+  def destroy
+    @content = Content.find(params[:content_id])
+    @answer = Answer.find_by(user_id: current_user.id, content_id: @content.id)
+    @answer.destroy
+  end
+
 end
