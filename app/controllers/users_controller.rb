d47c9b0668338d8f48357b83c11bcadfb437284b
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 13:02:27 +0900

    set_userを定義し記述をまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
 
   def show
-    @user = User.find(params[:id])
     @contents = @user.contents.order("created_at DESC").includes([:rich_text_question])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
