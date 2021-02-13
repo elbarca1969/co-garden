408aae0caabbe0ff10915c39dc784cd19680b30a
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 16:39:43 +0900

    answersコントローラーにcreateアクションを定義

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,8 @@
 class AnswersController < ApplicationController
+
+  def create
+    @content = Content.find(params[:content_id])
+    @answer = Answer.create(user_id: current_user.id, content_id: @content.id)
+  end
+
 end
