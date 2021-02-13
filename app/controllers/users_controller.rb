1429a61185ec83cdffef81d2ad7ca9dd02bdf184
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 13:01:23 +0900

    usersコントローラーにeditアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @contents = @user.contents.order("created_at DESC").includes([:rich_text_question])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
 end
