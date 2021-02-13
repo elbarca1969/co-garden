3645355a95e4b8a2f648b53c0a2d810e2f5716af
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 16:32:29 +0900

    answersテーブルの作成

--- db/migrate/20210213073045_create_answers.rb
+++ db/migrate/20210213073045_create_answers.rb
@@ -1,8 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.references :content, null: false, foreign_key: true
       t.timestamps
+      t.index [:user_id, :content_id], unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_140242) do
+ActiveRecord::Schema.define(version: 2021_02_13_073045) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2021_02_05_140242) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_answers_on_content_id"
+    t.index ["user_id", "content_id"], name: "index_answers_on_user_id_and_content_id", unique: true
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "grade_id", null: false
     t.integer "subject_id", null: false
@@ -72,5 +82,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_140242) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "contents"
+  add_foreign_key "answers", "users"
   add_foreign_key "contents", "users"
 end
