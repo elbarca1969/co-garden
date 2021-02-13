d5fe82c6c574de03bd2ca3dadd054269e35800d9
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 13:10:01 +0900

    usersコントローラーにupdateアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,18 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
   end
-  
+
 end
