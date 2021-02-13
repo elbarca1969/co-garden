9ad597fb3138c6b18902174b6695aae0d56cce22
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-13 16:37:27 +0900

    ルーティングにcreate, destroyアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "contents#index"
-  resources :contents
+  resources :contents do
+    resources :answers, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
