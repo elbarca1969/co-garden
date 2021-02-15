168e3c483736f9ef6225f214433fa7c5f4eb6b89
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-15 14:45:19 +0900

    contentsコントローラーのindexアクションを編集

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,8 @@ class ContentsController < ApplicationController
   before_action :set_content, only: [:show, :edit, :update, :destroy]
 
   def index
-    @contents = current_user.contents.order("created_at DESC").includes([:rich_text_question], :answers)
+    @q = current_user.contents.ransack(params[:q])
+    @contents = @q.result.order("created_at DESC").includes([:rich_text_question], :answers)
   end
 
   def new
