d7271097d673f88d62c402cfc5ee7d6262871870
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-15 15:46:18 +0900

    ルーティングにsearchアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "contents#index"
   resources :contents do
+    collection do
+      get :search
+    end
     resources :answers, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
