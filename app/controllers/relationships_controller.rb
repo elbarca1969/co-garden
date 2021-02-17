a202caeb6488afb955eda1cca087eb9c160def7c
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-17 16:53:09 +0900

    relationshipsコントローラーにcreateアクションを定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,10 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    @user = User.find(params[:follow_id])
+    following = current_user.follow(@user)
+    following.save
+    redirect_back(fallback_location: root_path)
+  end
+
 end
