25fb6854956992f384be30a69477de47fefd61bf
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-17 16:48:33 +0900

    userモデルにアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@ class User < ApplicationRecord
   has_many :contents
   has_many :answers, dependent: :destroy
 
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
