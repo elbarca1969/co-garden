5d947a7348bfc166c03478d20a7504b60ba04c17
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-18 13:59:22 +0900

    ルーティングにfollowing, followersアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
       get :search
     end
   end
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
 end
