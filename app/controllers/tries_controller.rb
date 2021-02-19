23117fb818a4f456d285064b781690d8731ec159
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-19 14:45:42 +0900

    set_contentを定義し記述をまとめる

--- app/controllers/tries_controller.rb
+++ app/controllers/tries_controller.rb
@@ -1,14 +1,19 @@
 class TriesController < ApplicationController
+  before_action :set_content, only: [:create, :destroy]
 
   def create
-    @content = Content.find(params[:content_id])
     @try = Try.create(user_id: current_user.id, content_id: @content.id)
   end
 
   def destroy
-    @content = Content.find(params[:content_id])
     @try = Try.find_by(user_id: current_user.id, content_id: @content.id)
     @try.destroy
+  end
+
+  private
+
+  def set_content
+    @content = Content.find(params[:content_id])
   end
 
 end
