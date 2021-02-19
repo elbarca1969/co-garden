953ce733fff35d94c766bcb8d42ec3168b4f477e
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-19 14:39:43 +0900

    userモデルにアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
 
   has_many :contents
   has_many :answers, dependent: :destroy
+  has_many :tries, dependent: :destroy
 
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
