81ede631e397828562a269afc9e60b2efcd19ac4
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-19 14:45:15 +0900

    triesコントローラーにdestroyアクションを定義

--- app/controllers/tries_controller.rb
+++ app/controllers/tries_controller.rb
@@ -5,4 +5,10 @@ class TriesController < ApplicationController
     @try = Try.create(user_id: current_user.id, content_id: @content.id)
   end
 
+  def destroy
+    @content = Content.find(params[:content_id])
+    @try = Try.find_by(user_id: current_user.id, content_id: @content.id)
+    @try.destroy
+  end
+
 end
