8fa48ff7fd9c893854e1c406c21332467876ba2a
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-19 14:44:43 +0900

    triesコントローラーにcreateアクションを定義

--- app/controllers/tries_controller.rb
+++ app/controllers/tries_controller.rb
@@ -1,2 +1,8 @@
 class TriesController < ApplicationController
+
+  def create
+    @content = Content.find(params[:content_id])
+    @try = Try.create(user_id: current_user.id, content_id: @content.id)
+  end
+
 end
