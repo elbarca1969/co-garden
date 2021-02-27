f9ef943cbdf42e16cda6b95f83448f9ec921906b
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-27 13:40:31 +0900

    commentsコントローラーにcreateアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @content = Content.find(params[:content_id])
+    @comment = @content.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      render :index
+    end
+  end
+  
 end
